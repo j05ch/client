33ac5b5b03caf31a6e76365452092a5da4c532e7
Josch <j.o.scholz@me.com>
2020-03-26 19:38:49 +0100

    add styles to Ads

--- src/components/Ads/index.tsx
+++ src/components/Ads/index.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import './index.scss';
 import axios from 'axios';
 import useApiUrlContext from "../../services/useApiUrl";
+import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
+interface CardObject {
+    id: number,
+    title: string,
+    imageUrl: string,
+    description: string
+    userId: number
+}
 
 const Ads: React.FunctionComponent<{}> = () => {
-    const [adList, setAdList] = useState<Array<Object>>([]);
+    const [adList, setAdList] = useState<Array<CardObject>>([]);
     const {apiUrl} = useApiUrlContext();
 
     useEffect(() => {
@@ -19,11 +31,23 @@ const Ads: React.FunctionComponent<{}> = () => {
         fetchData().catch(err => console.error(err));
     }, [apiUrl]);
 
+    function buildAdCards() {
+        return adList.map(ad =>
+            <Card className='card'>
+                <Card.Img variant='top' src={ad.imageUrl} />
+                <Card.Body>
+                    <Card.Title>{ad.title}</Card.Title>
+                    <Card.Text>{ad.description}</Card.Text>
+                    <Button as={Link} to={`/details:${ad.id}`} variant="primary">Details</Button>
+                </Card.Body>
+            </Card>
+        );
+    }
+
     return (
         <>
-            {console.log(adList)}
+            {buildAdCards()}
         </>
-
     );
 };
 
